3e8b76dedbfa0673faae1dd293fc04f6ef7d9916
Muvillo <ukulakova18@gmail.com>
2024-06-19 19:49:56 +0300

    Updates

--- src/Components/Menu/navMenu.js
+++
@@ -1,5 +0,0 @@
-export default function Button({name, link}){
-    return(
-        <a href={`#`+ link} key={name} className="btnMenu">{name}</a>
-    );
-}
--- src/Components/Portfolio/buttonPortfolio.js
+++
@@ -1,5 +0,0 @@
-export default function Button({name}){
-    return (
-        <button className="btnPortfolio"><a href="https://github.com/Muvillo"> {name} </a></button>
-    )
-}
